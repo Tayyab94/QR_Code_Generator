b871ea7ed818de7fd30d82f6976ee2c1f7cb77f0
tayyab94 <tayyab.bhatti30@gmail.com>
2021-09-10 11:13:51 -0700

    How to create QR code in dotnet core
    How to create QR code in dotnet core

---
+++ QR_Code_Generator/Controllers/HomeController.cs
@@ -0,0 +1,173 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using QR_Code_Generator.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using ZXing;
+using ZXing.QrCode;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace QR_Code_Generator.Controllers
+{
+
+
+        //    1. ZXing.Net
+        //PM> Install-Package ZXing.Net
+        //2. ZXing.Net.Bindings.CoreCompat.System.Drawing
+        //PM> Install-Package ZXing.Net.Bindings.CoreCompat.System.Drawing -Version 0.16.5-beta
+        //To work with images we will also need to install System.Drawing.Common package. It’s command is:
+        //Install-Package System.Drawing.Common
+    
+    
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Index(string qrText)
+        {
+            Byte[] byteArray;
+            var width = 250; // width of the Qr Code   
+            var height = 250; // height of the Qr Code   
+            var margin = 0;
+            var qrCodeWriter = new ZXing.BarcodeWriterPixelData
+            {
+                Format = ZXing.BarcodeFormat.QR_CODE,
+                Options = new QrCodeEncodingOptions
+                {
+                    Height = height,
+                    Width = width,
+                    Margin = margin
+                }
+            };
+            var pixelData = qrCodeWriter.Write(qrText);
+
+            // creating a bitmap from the raw pixel data; if only black and white colors are used it makes no difference   
+            // that the pixel data ist BGRA oriented and the bitmap is initialized with RGB   
+            using (var bitmap = new System.Drawing.Bitmap(pixelData.Width, pixelData.Height, System.Drawing.Imaging.PixelFormat.Format32bppRgb))
+            {
+                using (var ms = new MemoryStream())
+                {
+                    var bitmapData = bitmap.LockBits(new System.Drawing.Rectangle(0, 0, pixelData.Width, pixelData.Height), System.Drawing.Imaging.ImageLockMode.WriteOnly, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+                    try
+                    {
+                        // we assume that the row stride of the bitmap is aligned to 4 byte multiplied by the width of the image   
+                        System.Runtime.InteropServices.Marshal.Copy(pixelData.Pixels, 0, bitmapData.Scan0, pixelData.Pixels.Length);
+                    }
+                    finally
+                    {
+                        bitmap.UnlockBits(bitmapData);
+                    }
+                    // save to stream as PNG   
+                    bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                    byteArray = ms.ToArray();
+                }
+            }
+            return View(byteArray);
+        }
+
+
+        public IActionResult GenerateFile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GenerateFile(string qrText)
+        {
+            Byte[] byteArray;
+            var width = 250; // width of the Qr Code   
+            var height = 250; // height of the Qr Code   
+            var margin = 0;
+            var qrCodeWriter = new ZXing.BarcodeWriterPixelData
+            {
+                Format = ZXing.BarcodeFormat.QR_CODE,
+                Options = new QrCodeEncodingOptions
+                {
+                    Height = height,
+                    Width = width,
+                    Margin = margin
+                }
+            };
+            var pixelData = qrCodeWriter.Write(qrText);
+
+            // creating a bitmap from the raw pixel data; if only black and white colors are used it makes no difference   
+            // that the pixel data ist BGRA oriented and the bitmap is initialized with RGB   
+            using (var bitmap = new System.Drawing.Bitmap(pixelData.Width, pixelData.Height, System.Drawing.Imaging.PixelFormat.Format32bppRgb))
+            {
+                using (var ms = new MemoryStream())
+                {
+                    var bitmapData = bitmap.LockBits(new System.Drawing.Rectangle(0, 0, pixelData.Width, pixelData.Height), System.Drawing.Imaging.ImageLockMode.WriteOnly, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+                    try
+                    {
+                        // we assume that the row stride of the bitmap is aligned to 4 byte multiplied by the width of the image   
+                        System.Runtime.InteropServices.Marshal.Copy(pixelData.Pixels, 0, bitmapData.Scan0, pixelData.Pixels.Length);
+                    }
+                    finally
+                    {
+                        bitmap.UnlockBits(bitmapData);
+                    }
+
+                    // save to folder
+                    string fileGuid = Guid.NewGuid().ToString().Substring(0, 4);
+                    bitmap.Save("wwwroot/qrr/file-" + fileGuid + ".png", System.Drawing.Imaging.ImageFormat.Png);
+
+                    // save to stream as PNG   
+                    bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                    byteArray = ms.ToArray();
+                }
+            }
+            return View(byteArray);
+        }
+
+
+
+        public IActionResult ViewFile()
+        {
+            List<KeyValuePair<string, string>> fileData = new List<KeyValuePair<string, string>>();
+            KeyValuePair<string, string> data;
+            string[] files = Directory.GetFiles("wwwroot/qrr");
+            foreach (string file in files)
+            {
+                // create a barcode reader instance
+                BarcodeReader reader = new BarcodeReader();
+                // load a bitmap
+                var barcodeBitmap = (Bitmap)Image.FromFile("wwwroot/qrr/" + Path.GetFileName(file));
+                // detect and decode the barcode inside the bitmap
+                var result = reader.Decode(barcodeBitmap);
+                // do something with the result
+                data = new KeyValuePair<string, string>(result.ToString(), "/qrr/" + Path.GetFileName(file));
+                fileData.Add(data);
+
+            }
+            return View(fileData);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ QR_Code_Generator/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace QR_Code_Generator.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
